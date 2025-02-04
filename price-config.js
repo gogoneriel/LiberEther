f0bcc48f127945f3ec5a18fb04ab11ce4044a55a
Neriel <124447452+gogoneriel@users.noreply.github.com>
2025-02-04 15:09:25 -0300

    price updated manually

--- price-config.js
+++ price-config.js
@@ -1,4 +1,4 @@
 const priceConfig = {
-    currentPrice: 0.44,  // Change this number to update the price
+    currentPrice: 0.46,  // Change this number to update the price
     lastUpdate: Date.now() // This helps bypass cache
 };
